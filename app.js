45954e5eb3b41486245b8346cac3b7e411a416d7
Tayo Carvalhal <lucarv@lucarv-2.local>
2018-10-11 00:44:03 -0700

    added adal-node

--- app.js
+++ app.js
@@ -1,65 +1,59 @@
-﻿'use strict';
-var debug = require('debug');
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+
+var authObj = require("./Auth.js").Create({
+    tenant:"aadnodejstest.onmicrosoft.com",
+    clientId:"edce4438-2a8f-4da4-93dc-41e3646fdf56",
+    secret:"Es8bqpLtZalpXhOdK/pERgSz+dM48M7hoEkzV5Udgzo=",
+    redirectUri:"https://aad-nodejs-test3.azurewebsites.net/getAToken"
+});
 
 var app = express();
+app.use(cookieParser('a deep secret'));
+app.use(cookieSession({name: 'session',keys: [""]}));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.get('/', function(req, res) {
+    res.end('\
+        <head>\
+        <title>test</title>\
+        </head>\
+        <body>\
+        <h3>dummy login page</h3>\
+        <a href="./auth">Login</a>\
+        </body>\
+    ');
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+app.get('/auth', function(req, res) {
+    authObj.loginIfNotAuth(req,res,function(){
+        res.send("authed");
     });
 });
 
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
+app.get('/getAToken', function(req, res) {
+    authObj.receiveToken(req,res,function(){
+        res.redirect('/AuthInfo');
+    });
 });
+
+app.get('/AuthInfo', function(req, res) {
+    var sessionValue = req.session.authInfo;
+    var authString = JSON.stringify(sessionValue);
+    var userID = sessionValue.userId;
+    var familyName = sessionValue.familyName;
+    var givenName = sessionValue.givenName;
+
+    res.end(`\
+        <h1>UserID: ${userID}</h1>
+        <h2>familyName: ${familyName}</h2>
+        <h2>givenName: ${givenName}</h2>
+        <h2>full data:</h2>
+        <p>${authString}</p>
+    `);
+});
+var port = process.env.port || 3000;
+app.listen(port);
+console.log("listen 3000");
